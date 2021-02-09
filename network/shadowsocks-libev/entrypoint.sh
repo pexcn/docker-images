1da3076c618c85e6de45820833d9ffb9921b5cc9
pexcn <i@pexcn.me>
2021-02-09 10:22:27 +0800

    network/shadowsocks-libev: support other modes

--- network/shadowsocks-libev/entrypoint.sh
+++ network/shadowsocks-libev/entrypoint.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
 
-exec /app/bin/ss-server \
-  -s $SERVER_HOST \
-  -p $SERVER_PORT \
-  -k $PASSWORD \
-  -m $METHOD \
-  -t $TIMEOUT \
-  -n $NOFILE \
-  -d $DNS_SERVERS \
-  -u \
-  --mtu $MTU \
-  --fast-open \
-  --reuse-port \
-  --no-delay
+if [[ -n "$SERVER_HOST" && -n "$SERVER_PORT" && -n "$PASSWORD" ]]; then
+  exec /app/bin/ss-server \
+    -s $SERVER_HOST \
+    -p $SERVER_PORT \
+    -k $PASSWORD \
+    -m $METHOD \
+    -t $TIMEOUT \
+    -n $NOFILE \
+    -d $DNS_SERVERS \
+    -u \
+    --mtu $MTU \
+    --fast-open \
+    --reuse-port \
+    --no-delay \
+    $ARGS
+else
+  exec /app/bin/$@
+fi
