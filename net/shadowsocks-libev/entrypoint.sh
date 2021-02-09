4a120a1783c3e59cd5c32bc7db40050dd5244c06
pexcn <i@pexcn.me>
2021-02-09 21:21:18 +0800

    net/shadowsocks-libev: support plugins

--- net/shadowsocks-libev/entrypoint.sh
+++ net/shadowsocks-libev/entrypoint.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 
 if [[ -n "$SERVER_HOST" && -n "$SERVER_PORT" && -n "$PASSWORD" ]]; then
+  if [[ -n "$PLUGIN" ]]; then
+    ARGS_EXTRA="--plugin $PLUGIN"
+    if [[ -n "$PLUGIN_OPTS" ]]; then
+      ARGS_EXTRA="$ARGS_EXTRA --plugin-opts $PLUGIN_OPTS"
+    fi
+  fi
+  if [[ -n "$ARGS_EXTRA" ]]; then
+    ARGS="$ARGS_EXTRA $ARGS"
+  fi
   exec /app/bin/ss-server \
     -s $SERVER_HOST \
     -p $SERVER_PORT \
