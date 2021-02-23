3ba22940d883332e88e5a5758ab9738d5f1f2554
pexcn <i@pexcn.me>
2021-02-23 14:46:45 +0800

    utils/smartmontools: support send warning email

--- utils/smartmontools/entrypoint.sh
+++ utils/smartmontools/entrypoint.sh
@@ -1,9 +1,44 @@
 #!/bin/sh
 
-if [[ -n "$SMARTD_CONFIG_PARAMS" && "$SMARTD_CONFIG_PARAMS" != "DEVICESCAN" ]]; then
-  echo $SMARTD_CONFIG_PARAMS > /etc/smartd.conf
-fi
+update_smartd_conf() {
+  if [[ -n "$SMARTD_PARAMS" && "$SMARTD_PARAMS" != "DEVICESCAN" ]]; then
+    echo $SMARTD_PARAMS > /etc/smartd.conf
+  fi
+}
 
-cat /etc/smartd.conf
+create_msmtprc() {
+  local host=$(echo $SMTP_CONFIG | cut -d '@' -f 3 | cut -d ':' -f 1)
+  local port=$(echo $SMTP_CONFIG | cut -d ':' -f 3)
+  local from=$(echo $SMTP_CONFIG | cut -d '#' -f 1)
+  local user=$(echo $SMTP_CONFIG | cut -d '#' -f 2 | cut -d ':' -f 1)
+  local password=$(echo $SMTP_CONFIG | cut -d ':' -f 2 | cut -d '@' -f 1)
 
-#exec /app/smartd --no-fork
+  cat <<- EOF > /etc/msmtprc
+	defaults
+	auth on
+	tls on
+	#tls_starttls off
+	tls_trust_file /etc/ssl/certs/ca-certificates.crt
+	logfile ~/.msmtp.log
+
+	account provider
+	host $host
+	port $port
+	from $from
+	user $user
+	password $password
+
+	account default: provider
+	EOF
+
+  # if port equal to 465, must be disable STARTTLS
+  # https://wiki.archlinux.org/index.php/Msmtp#Server_sent_empty_reply
+  [ "$port" == 465 ] && sed -i '/tls_starttls/s/^#//' /etc/msmtprc
+
+  chmod 600 /etc/msmtprc
+}
+
+update_smartd_conf
+create_msmtprc
+
+exec /app/smartd --no-fork
