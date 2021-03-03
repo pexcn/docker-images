2cf91e999864477ea3f7148c44793d5f73277bae
pexcn <i@pexcn.me>
2021-03-04 04:00:46 +0800

    add pptpd

---
+++ net/pptpd/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+sysctl -w net.ipv4.ip_forward=1
+iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+
+# fix mtu
+[ "$FIX_MTU" == 1 ] && iptables -A FORWARD -p tcp --syn -s 192.168.10.0/24 -j TCPMSS --set-mss 1356
+
+exec pptpd -f
