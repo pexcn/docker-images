4d3541dd8507d85ba1715606c477068b7072766f
pexcn <i@pexcn.me>
2021-03-05 00:18:20 +0800

    update

--- net/pptpd/entrypoint.sh
+++ net/pptpd/entrypoint.sh
@@ -7,11 +7,24 @@ if [ -n "$AUTH" ]; then
   done
 fi
 
-# iptables setting
+# configure firewall
 sysctl -w net.ipv4.ip_forward=1
-iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+#iptables -t nat -A POSTROUTING -o enp4s0 -j MASQUERADE
+#iptables -t nat -A POSTROUTING -s 192.168.10.0/24 ! -d 192.168.10.0/24 -j MASQUERADE
+#iptables -A INPUT -i ppp+ -j ACCEPT
+#iptables -A OUTPUT -o ppp+ -j ACCEPT
+#iptables -A INPUT -p tcp --dport 1723 -j ACCEPT
+#iptables -A INPUT -p 47 -j ACCEPT
+#iptables -A OUTPUT -p 47 -j ACCEPT
+#iptables -F FORWARD
+#iptables -A FORWARD -j ACCEPT
+#iptables -A POSTROUTING -t nat -o enp4s0 -j MASQUERADE
+#iptables -A POSTROUTING -t nat -o ppp+ -j MASQUERADE
 
 # fix mtu
-[ "$FIX_MTU" == 1 ] && iptables -A FORWARD -p tcp --syn -s 192.168.10.0/24 -j TCPMSS --set-mss 1356
+if [ "$FIX_MTU" == 1 ]; then
+  iptables -A FORWARD -p tcp --syn -s 192.168.10.0/24 -j TCPMSS --set-mss 1356
+  #iptables -A FORWARD -s 192.168.10.0/24 -p tcp -m tcp --tcp-flags FIN,SYN,RST,ACK SYN -j TCPMSS --set-mss 1356
+fi
 
 exec pptpd -f
