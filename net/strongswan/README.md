f3745e57e31f9c522a8b960bcb21a73be428c2b6
pexcn <i@pexcn.me>
2021-04-06 23:45:55 +0800

    update

--- net/strongswan/README.md
+++ net/strongswan/README.md
@@ -7,6 +7,7 @@
 docker run -d \
   --name strongswan \
   --restart unless-stopped \
+  -e PSK="psk" \
   -v /etc/localtime:/etc/localtime:ro \
   pexcn/docker-images:strongswan
 ```
--- net/strongswan/entrypoint.sh
+++ net/strongswan/entrypoint.sh
@@ -1,8 +1,51 @@
-%any %any : PSK "passwd"
-user %any : EAP "passwd"
+#!/bin/sh
 
-: XAUTH "passwd"
-%any %any : EAP "passwd"
+clear_config() {
+  > /etc/ipsec.secrets
+  > /etc/ipsec.conf
+}
 
-test : XAUTH "user password"
-test : EAP "user password"
+gen_ipsec_secrets() {
+  [ -n "$PSK" ] && echo "%any %any : PSK \"$PSK\"" >> /etc/ipsec.secrets
+}
+
+gen_ipsec_conf() {
+  # common
+  cat <<- EOF >> /etc/ipsec.conf
+	config setup
+	    uniqueids=no
+  EOF
+
+  # l2tp-psk
+  cat <<- EOF >> /etc/ipsec.conf
+	conn l2tp-psk
+	    left=%defaultroute
+	    right=%any
+	    leftprotoport=17/1701
+	    rightprotoport=17/1701
+	    authby=secret
+	    type=transport
+	    dpdaction=clear
+	    rekey=no
+	    auto=add
+  EOF
+
+  # l2tp-xauth
+  # ikev2
+}
+
+build_config() {
+  clear_config
+  gen_ipsec_secrets
+  gen_ipsec_conf
+}
+
+
+
+sysctl -w net.ipv4.ip_forward=1
+sysctl -w net.ipv4.conf.all.accept_redirects=0
+sysctl -w net.ipv4.conf.all.send_redirects=0
+
+build_config
+
+ipsec start
