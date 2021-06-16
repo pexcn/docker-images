feff702e712675732a9cb6e5e68570eb7f28f138
pexcn <i@pexcn.me>
2021-06-16 16:03:09 +0800

    refine upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -6,6 +6,7 @@ for dir in $(find . -name "docker-compose.yml" | xargs dirname); do
   cd $dir
   docker-compose pull --include-deps
   docker-compose up -d --remove-orphans
-  docker image prune -af
   cd $BASE_DIR
 done
+
+docker image prune -af
