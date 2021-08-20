0e11d9006b6539c2475bc6a1d598f628ee7a635f
pexcn <i@pexcn.me>
2021-08-20 23:55:16 +0800

    mtg: simple run only

--- net/mtg/entrypoint.sh
+++ net/mtg/entrypoint.sh
@@ -1,13 +1,4 @@
 #!/bin/sh
+set -e
 
-if [ -n "$BIND_TO" ]; then
-  sed -i "s/bind-to = \"0.0.0.0:3128\"/bind-to = \"$BIND_TO\"/" /etc/mtg/config.toml
-fi
-if [ -n "$SECRET" ]; then
-  sed -i "s/secret = \"ee367a189aee18fa31c190054efd4a8e9573746f726167652e676f6f676c65617069732e636f6d\"/secret = \"$SECRET\"/" /etc/mtg/config.toml
-fi
-if [ -n "$TCP_BUFFER" ]; then
-  sed -i "s/tcp-buffer = \"4kb\"/tcp-buffer = \"$TCP_BUFFER\"/" /etc/mtg/config.toml
-fi
-
-exec mtg run /etc/mtg/config.toml
+exec mtg simple-run "$@"
