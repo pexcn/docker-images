47c081ea6627ef9957cf5e3b35574b23f6f78212
pexcn <i@pexcn.me>
2021-12-26 03:54:41 +0800

    samba: refine dockerfile

--- utils/samba/samba.sh
+++ utils/samba/samba.sh
@@ -7,8 +7,8 @@ create_user() {
     for account in ${USERS//,/ }; do
       local username=$(echo $account | cut -d ':' -f 1)
       local password=$(echo $account | cut -d ':' -f 2)
-      adduser -H -D -s /sbin/nologin $username
-      printf "%s\n%s\n" $password $password | pdbedit -a $username -f "Samba User" -t
+      adduser -D -H -s /sbin/nologin $username
+      echo -e "$password\n$password" | pdbedit -a $username -f "Samba User" -t
     done
   fi
 }
