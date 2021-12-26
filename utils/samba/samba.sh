a0e1159075ea9e73cb64e8b58506accc56ea29a5
pexcn <i@pexcn.me>
2021-12-26 16:59:32 +0800

    samba: refine create group

--- utils/samba/samba.sh
+++ utils/samba/samba.sh
@@ -2,17 +2,20 @@
 set -e
 set -o pipefail
 
-_is_exist_user() {
-  id -u $1 &>/dev/null
-}
-
 _is_exist_group() {
   id -g $1 &>/dev/null
 }
 
+_is_exist_user() {
+  id -u $1 &>/dev/null
+}
+
+create_group() {
+  _is_exist_group samba || addgroup -g 1000 samba
+}
+
 create_user() {
   if [ -n "$USERS" ]; then
-    _is_exist_group samba || addgroup -g 2000 samba
     for account in ${USERS//,/ }; do
       local username=$(echo $account | cut -d ':' -f 1)
       local password=$(echo $account | cut -d ':' -f 2)
@@ -28,5 +31,6 @@ start_samba() {
   exec smbd --foreground --no-process-group
 }
 
+create_group
 create_user
 start_samba
