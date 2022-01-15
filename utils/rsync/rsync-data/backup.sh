fe0819551a6b052e890320c4a2d32e2aa6bc8191
pexcn <i@pexcn.me>
2022-01-16 00:46:21 +0800

    rsync: update cron script template

--- utils/rsync/rsync-data/backup.sh
+++ utils/rsync/rsync-data/backup.sh
@@ -11,7 +11,7 @@ echo
 #rsync -avz --delete /root/docker-compose/vaultwarden /mnt/disk/backup
 
 # daemon pull
-#rsync -avz --delete user1@192.168.1.10::source /mnt/disk/backup --password-file=<(cat /etc/rsyncd.secrets | cut -d ':' -f 2)
+#rsync -avz --delete user1@192.168.1.10::source /mnt/disk/backup/docker-compose --password-file=<(cat /etc/rsyncd.secrets | cut -d ':' -f 2)
 
 echo
 echo "[$(date '+%Y-%m-%d %T.%3N')] Backup End."
