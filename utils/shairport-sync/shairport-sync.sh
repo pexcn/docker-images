29f0110090d3be7ea41309432b13ca98c8088ef3
pexcn <i@pexcn.me>
2022-02-12 17:13:18 +0800

    shairport-sync: enable sound card

--- utils/shairport-sync/shairport-sync.sh
+++ utils/shairport-sync/shairport-sync.sh
@@ -6,11 +6,26 @@ _is_exist_group() {
   getent group $1 &>/dev/null
 }
 
+_set_volume() {
+  local mixer="$1"
+  local volume="$2"
+  if amixer -c $CARD_NUM -M sget $mixer &>/dev/null; then
+    amixer -c $CARD_NUM -M sset $mixer unmute
+    amixer -c $CARD_NUM -M sset $mixer ${volume}%
+  fi
+}
+
 add_audio_group() {
   if ! _is_exist_group docker-audio; then
     addgroup -S -g $AUDIO_GID docker-audio && adduser airplay docker-audio
-    echo add-audio-group...
   fi
+}
+
+enable_sound() {
+  _set_volume 'Master' 80
+  _set_volume 'Headphone' 100
+  _set_volume 'Speaker' 100
+  _set_volume 'Line Out' 100
 }
 
 start_depends() {
@@ -20,6 +35,11 @@ start_depends() {
   avahi-daemon --daemonize --no-rlimits --no-chroot
 }
 
+start_process() {
+  exec su airplay -c shairport-sync "$@"
+}
+
 add_audio_group
+enable_sound
 start_depends
-exec su airplay -c shairport-sync "$@"
+start_process
