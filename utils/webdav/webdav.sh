99395a0a126874e4f37a0fb587059696dfd460a4
pexcn <i@pexcn.me>
2022-02-19 20:44:31 +0800

    webdav: run as specific UID/GID to have correct permissions

--- utils/webdav/webdav.sh
+++ utils/webdav/webdav.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 set -e
 
-exec webdav "$@"
+_is_exist_group() {
+  getent group $1 &>/dev/null
+}
+
+_is_exist_user() {
+  id -u $1 &>/dev/null
+}
+
+_is_exist_group webdav || addgroup -S -g $GID webdav
+_is_exist_user webdav || adduser -S -s /bin/ash -u $UID webdav
+exec su webdav -c webdav "$@"
