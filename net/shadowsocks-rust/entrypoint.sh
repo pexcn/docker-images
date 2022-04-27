34e1dcfbaa78fae0f5ac7768eca27ab993f175db
pexcn <i@pexcn.me>
2022-04-27 18:19:54 +0800

    ss-rust: add CHILLING_EFFECT option

---
+++ net/shadowsocks-rust/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -e
+
+if [ "$CHILLING_EFFECT" == 1 ]; then
+  exec $(echo "$@" | sed '
+    s/ssservice/service/;
+    s/ssserver/server/;
+    s/sslocal/local/;
+    s/ssmanager/manager/;
+    s/ssurl/url/;
+    s/v2ray-plugin/pv-plugin/;
+    s/xray-plugin/px-plugin/;
+    s/obfs-server/mix-server/;
+    s/obfs-local/mix-local/
+  ')
+else
+  exec "$@"
+fi
