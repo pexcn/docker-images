65453823747bc5ee6882f3c9e4ed60938b9003c2
pexcn <i@pexcn.me>
2022-05-22 00:59:49 +0800

    dev: add rust-lang development environment draft for vscode

---
+++ dev/rust/rust.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+set -e
+
+_graceful_stop() {
+  echo "Container stopped."
+  exit 0
+}
+
+start_container() {
+  echo "Container started."
+
+  # Cause: /bin/sh -> dash
+  # Debian dash shell doesn't recognize SIGTERM signal, use signal number 15 instead.
+  trap _graceful_stop 15
+
+  #exec "$@"
+  sleep infinity &
+  wait
+}
+
+start_container
