651c6f9e76298f6c6cfa3169b459465b8112c95e
pexcn <i@pexcn.me>
2022-07-20 18:16:23 +0800

    rust: support for setup git global config

--- dev/rust/rust.sh
+++ dev/rust/rust.sh
@@ -6,6 +6,20 @@ _graceful_stop() {
   exit 0
 }
 
+git_global_config() {
+  local user=$(git config --global --get user.name)
+  local email=$(git config --global --get user.email)
+  local non_root_user="vscode"
+  if [[ -n "$GIT_USER" && "$GIT_USER" != "$user" ]]; then
+    git config --global user.name "$GIT_USER"
+    su $non_root_user -c "git config --global user.name $GIT_USER"
+  fi
+  if [[ -n "$GIT_EMAIL" && "$GIT_EMAIL" != "$email" ]]; then
+    git config --global user.email "$GIT_EMAIL"
+    su $non_root_user -c "git config --global user.email $GIT_EMAIL"
+  fi
+}
+
 start_container() {
   echo "Container started."
 
@@ -20,4 +34,5 @@ start_container() {
   wait
 }
 
+git_global_config
 start_container
