37ac8931c1bd0702e970a81de1678b03406b93b7
pexcn <i@pexcn.me>
2022-07-23 01:27:48 +0800

    ss-rust: workaround for acme.sh deploy auto reload
    ref:
    https://github.com/shadowsocks/shadowsocks-rust/issues/748
    https://github.com/acmesh-official/acme.sh/wiki/deploy-to-docker-containers

---
+++ net/shadowsocks-rust/restart-container.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+#_get_container_id_by_name() {
+#  local container_name="$1"
+#  curl --silent --unix-socket /var/run/docker.sock -X GET "http://localhost/containers/json?filters=%7B%22name%22%3A%5B%22${container_name}%22%5D%7D" |
+#    tr '{,' '\n' |
+#    grep -i '"id":' |
+#    head -n 1 |
+#    cut -d '"' -f 4
+#}
+
+#_restart_container_by_id() {
+#  local container_name="$1"
+#  local container_id="$(_get_container_id_by_name $container_name)"
+#  curl --silent --unix-socket /var/run/docker.sock -X POST http://localhost/containers/${container_id}/restart
+#}
+
+_restart_container_by_name() {
+  local container_name="$1"
+  curl --silent --unix-socket /var/run/docker.sock -X POST http://localhost/containers/${container_name}/restart
+}
+
+_restart_container_by_name "$1"
