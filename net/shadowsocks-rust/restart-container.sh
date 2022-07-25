e6d092bdbfccb093f709ca131f83b1ce5d91e7a3
pexcn <i@pexcn.me>
2022-07-25 16:19:23 +0800

    ss-rust: refine restart-container.sh

--- net/shadowsocks-rust/restart-container.sh
+++ net/shadowsocks-rust/restart-container.sh
@@ -1,23 +1,30 @@
 #!/bin/sh
+#
+# The design idea was derived from docker.sh by acmesh-official
+#
+# https://github.com/acmesh-official/acme.sh/blob/5b8d7a3/deploy/docker.sh
+#
 
-#_get_container_id_by_name() {
-#  local container_name="$1"
-#  curl --silent --unix-socket /var/run/docker.sock -X GET "http://localhost/containers/json?filters=%7B%22name%22%3A%5B%22${container_name}%22%5D%7D" |
-#    tr '{,' '\n' |
-#    grep -i '"id":' |
-#    head -n 1 |
-#    cut -d '"' -f 4
-#}
+DOCKER_DAEMON_SOCKET="/var/run/docker.sock"
 
-#_restart_container_by_id() {
-#  local container_name="$1"
-#  local container_id="$(_get_container_id_by_name $container_name)"
-#  curl --silent --unix-socket /var/run/docker.sock -X POST http://localhost/containers/${container_id}/restart
-#}
+_get_container_id_by_name() {
+  local container_name="$1"
+  curl --silent --unix-socket "$DOCKER_DAEMON_SOCKET" -X GET "http://localhost/containers/json?filters=%7B%22name%22%3A%5B%22${container_name}%22%5D%7D" |
+    tr '{,' '\n' |
+    grep -i '"id":' |
+    head -n 1 |
+    cut -d '"' -f 4
+}
+
+_restart_container_by_id() {
+  local container_name="$1"
+  local container_id="$(_get_container_id_by_name $container_name)"
+  curl --silent --unix-socket "$DOCKER_DAEMON_SOCKET" -X POST http://localhost/containers/${container_id}/restart
+}
 
 _restart_container_by_name() {
   local container_name="$1"
-  curl --silent --unix-socket /var/run/docker.sock -X POST http://localhost/containers/${container_name}/restart
+  curl --silent --unix-socket "$DOCKER_DAEMON_SOCKET" -X POST http://localhost/containers/${container_name}/restart
 }
 
 for container_name in "$@"; do
