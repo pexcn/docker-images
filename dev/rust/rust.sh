87c93f305530410aa0f4a003d7e37426ec11ed83
pexcn <i@pexcn.me>
2022-07-30 22:26:18 +0800

    rust: fix rust environment variables missing in ssh session
    reference: https://github.com/rust-lang/docker-rust/blob/e3ad1f7/1.62.1/bullseye/slim/Dockerfile#L3-L6
    
    NOTE:
    In this case, can not set `SetEnv` in sshd_config, because the `PATH` environment variable will override by /etc/profile.

--- dev/rust/rust.sh
+++ dev/rust/rust.sh
@@ -43,6 +43,15 @@ ssh_daemon_config() {
     fi
   fi
 
+  if [ ! -f /etc/profile.d/env_rust.sh ]; then
+    cat <<- EOF > /etc/profile.d/env_rust.sh
+	export RUSTUP_HOME=$RUSTUP_HOME \\
+	  CARGO_HOME=$CARGO_HOME \\
+	  PATH=/usr/local/cargo/bin:\$PATH \\
+	  RUST_VERSION=$RUST_VERSION
+	EOF
+  fi
+
   service ssh start
 }
 
