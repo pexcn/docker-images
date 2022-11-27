6e20a85143f6bf2ac22b83988d9aaf5b1bec5593
pexcn <i@pexcn.me>
2022-11-27 17:23:37 +0800

    motd: go!

--- civ/motd/motd.sh
+++ civ/motd/motd.sh
@@ -1,28 +1,44 @@
-#!/bin/sh
+#!/bin/bash
 
 info() {
   local green='\e[0;32m'
   local clear='\e[0m'
   local time=$(date '+%Y-%m-%d %T')
-  printf "${green}[${time}] [INFO]: ${clear}%s\n" "$*"
+  printf "${green}[${time}] [INFO]: ${clear}%b\n" "$*"
 }
 
-#lastlog_watcher() {
-#}
+warn() {
+  local yellow='\e[1;33m'
+  local clear='\e[0m'
+  local time=$(date '+%Y-%m-%d %T')
+  printf "${yellow}[${time}] [WARN]: ${clear}%s\n" "$*" >&2
+}
 
-#graceful_stop() {
-#  warn "caught SIGTERM or SIGINT signal, graceful stopping..."
-#}
+login_callback() {
+  # make sure message queue not empty
+  if [ ${#queue[@]} = 0 ]; then
+    readarray -d '' queue < <(find /srv/messages -maxdepth 1 -type f -print0 | shuf -z)
+    [ ${#queue[@]} != 0 ] || { warn "motd message not found. skip."; return; }
+    info "motd message queue filled: ${queue[@]}"
+  fi
+
+  # motd = header + message
+  local message=${queue[0]}
+  info "next login will show: $message"
+  (
+    [ -z "$MESSAGE_HEADER" ] || printf "${MESSAGE_HEADER}\n"
+    cat $message <(echo)
+  ) > /etc/motd
+
+  # poll element from queue
+  queue=("${queue[@]:1}")
+}
 
 start_motd() {
-  while true; do
-    echo TODO.
+  inotifyd - /var/log/lastlog:c | while read line; do
+    info "user logged in, trigger callback."
+    login_callback
   done
-
-  #inotifyd - /var/log/lastlog:c | while read line; do
-  #  local file=$(echo $line | awk '{print $2}')
-  #  echo $file changed.
-  #done
 }
 
 start_motd
