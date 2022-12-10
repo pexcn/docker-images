cb6d81577666c9f4b9e121bfd46d926819f8c623
pexcn <i@pexcn.me>
2022-12-11 00:57:16 +0800

    phantun: refine code

--- net/phantun/phantun.sh
+++ net/phantun/phantun.sh
@@ -67,8 +67,21 @@ _get_peer_from_args() {
   _is_server_mode "$1" && echo ${peer:=192.168.201.2} || echo ${peer:=192.168.200.2}
 }
 
-_get_localport_from_args() {
+_get_port_from_args() {
   echo "$@" | awk -F '-l|--local' '{print $2}' | awk '{print $1}'
+}
+
+_stop_process() {
+  kill $(pidof phantun-server phantun-client)
+  info "terminate phantun process."
+}
+
+_revoke_iptables() {
+  local tun=$(_get_tun_from_args "$@")
+  local port=$(_get_port_from_args "$@")
+  local comment="phantun_${tun}_${port}"
+  iptables-save | grep -v "${comment}" | iptables-restore
+  info "remove iptables rule: [${comment}]."
 }
 
 apply_sysctl() {
@@ -76,13 +89,13 @@ apply_sysctl() {
   _is_ipv4_only "$@" || info "apply sysctl: $(sysctl -w net.ipv6.conf.all.forwarding=1)"
 }
 
-setup_iptables() {
+apply_iptables() {
   local interface=$(_get_default_iface)
   local address=$(_get_addr_by_iface "${interface}")
   local tun=$(_get_tun_from_args "$@")
   local peer=$(_get_peer_from_args "$@")
-  local localport=$(_get_localport_from_args "$@")
-  local comment="phantun_${tun}_${localport}"
+  local port=$(_get_port_from_args "$@")
+  local comment="phantun_${tun}_${port}"
 
   if _check_rule_by_comment "${comment}"; then
     warn "iptables rule already exist, maybe needs to check."
@@ -91,7 +104,7 @@ setup_iptables() {
     iptables -A FORWARD -o $tun -j ACCEPT -m comment --comment "${comment}"
     if _is_server_mode "$1"; then
       info "add iptables DNAT rule: [${comment}]: ${interface} -> ${tun}, ${address} -> ${peer}"
-      iptables -t nat -A PREROUTING -p tcp -i $interface --dport $localport -j DNAT --to-destination $peer \
+      iptables -t nat -A PREROUTING -p tcp -i $interface --dport $port -j DNAT --to-destination $peer \
         -m comment --comment "${comment}" || error "iptables DNAT rule add failed."
     else
       info "add iptables SNAT rule: [${comment}]: ${tun} -> ${interface}, ${peer} -> ${address}"
@@ -103,18 +116,14 @@ setup_iptables() {
 
 graceful_stop() {
   warn "caught SIGTERM or SIGINT signal, graceful stopping..."
-
-  local tun=$(_get_tun_from_args "$@")
-  local localport=$(_get_localport_from_args "$@")
-  local comment="phantun_${tun}_${localport}"
-  iptables-save | grep -v "${comment}" | iptables-restore
-  info "remove iptables rule: [${comment}]"
+  _stop_process
+  _revoke_iptables "$@"
 }
 
 start_phantun() {
   trap 'graceful_stop "$@"' SIGTERM SIGINT
   apply_sysctl "$@"
-  setup_iptables "$@"
+  apply_iptables "$@"
   "$@" &
   wait
 }
