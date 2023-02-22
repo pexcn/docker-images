ce5ea12540d0f67a8e9e586e8b3ff49688fae9cd
pexcn <pexcn97@gmail.com>
2023-02-22 22:07:09 +0800

    update gfw-defense

--- civ/gfw-defense/gfw-defense.sh
+++ civ/gfw-defense/gfw-defense.sh
@@ -21,8 +21,15 @@ setup_iptables() {
   #DEFAULT_POLICY="RETURN"
 
   iptables -N GFW_DEFENSE
-  iptables -A GFW_DEFENSE -m set --match-set $WHITELIST src -j RETURN
-  iptables -A GFW_DEFENSE -m set --match-set $BLACKLIST src -j "$BLOCKING_POLICY"
+
+  if [ "$BLACKLIST_FIRST" = 1 ]; then
+    iptables -A GFW_DEFENSE -m set --match-set $BLACKLIST src -j "$BLOCKING_POLICY"
+    iptables -A GFW_DEFENSE -m set --match-set $WHITELIST src -j RETURN
+  else
+    iptables -A GFW_DEFENSE -m set --match-set $WHITELIST src -j RETURN
+    iptables -A GFW_DEFENSE -m set --match-set $BLACKLIST src -j "$BLOCKING_POLICY"
+  fi
+
   iptables -A GFW_DEFENSE -j "$DEFAULT_POLICY"
   iptables -I INPUT -j GFW_DEFENSE
 }
