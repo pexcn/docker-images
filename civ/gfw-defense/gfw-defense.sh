e4a48e2bf31691fe16aff0ac818cc7a660ad742d
pexcn <pexcn97@gmail.com>
2023-02-22 22:03:42 +0800

    update gfw-defense

--- civ/gfw-defense/gfw-defense.sh
+++ civ/gfw-defense/gfw-defense.sh
@@ -2,30 +2,34 @@
 
 # shellcheck disable=SC2155,SC3043
 
-load_rules() {
-  local blacklist="blacklist"
-  local whitelist="whitelist"
-  ipset create $blacklist hash:net hashsize 64 family inet
-  ipset create $whitelist hash:net hashsize 64 family inet
+# WHITELIST=/etc/gfw-defense/whitelist.txt (some-cidr)
+# BLACKLIST=/etc/gfw-defense/blacklist.txt (chnroute exclude some-cidr)
+
+WHITELIST="gfw_defense_whitelist"
+BLACKLIST="gfw_defense_blacklist"
+
+load_ipset() {
+  ipset create $WHITELIST hash:net hashsize 64 family inet
+  ipset create $BLACKLIST hash:net hashsize 64 family inet
 }
 
-todo() {
-  WHITELIST="/etc/gfw-defense/whitelist.txt"
-  BLACKLIST="/etc/gfw-defense/blacklist.txt"
-
+setup_iptables() {
   # DROP or REJECT
-  BLOCKING_POLICY="DROP"
+  #BLOCKING_POLICY="DROP"
   # MODE=whitelist -> $BLOCKING_POLICY
   # MODE=blacklist -> RETURN
-  DEFAULT_POLICY="RETURN"
+  #DEFAULT_POLICY="RETURN"
 
   iptables -N GFW_DEFENSE
-  iptables -A GFW_DEFENSE -m set --match-set $GFW_DEFENSE_WHITELIST src -j RETURN
-  iptables -A GFW_DEFENSE -m set --match-set $GFW_DEFENSE_BLACKLIST src -j $BLOCKING_POLICY
-  iptables -A GFW_DEFENSE -j $DEFAULT_POLICY
+  iptables -A GFW_DEFENSE -m set --match-set $WHITELIST src -j RETURN
+  iptables -A GFW_DEFENSE -m set --match-set $BLACKLIST src -j "$BLOCKING_POLICY"
+  iptables -A GFW_DEFENSE -j "$DEFAULT_POLICY"
   iptables -I INPUT -j GFW_DEFENSE
 }
 
 start_gfw_defense() {
-  echo TODO.
+  load_ipset
+  setup_iptables
+
+  # TODO.
 }
