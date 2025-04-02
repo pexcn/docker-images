30a01cb0cf13a332548d2ed1b4cb789900ed73df
pexcn <pexcn97@gmail.com>
2025-04-02 11:40:42 +0800

    dev: go/rust: use android way to fix environment variables

--- dev/go/go.sh
+++ dev/go/go.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
-# fix go environment variables in ssh session
 [ "$ENABLE_SSHD" = 1 ] || exit 0
+
+# fix environment variables in ssh session
 if [ ! -f /etc/profile.d/env_go.sh ]; then
   cat <<- EOF >/etc/profile.d/env_go.sh
-	export PATH=/usr/local/go/bin:\$PATH \\
+	export PATH=$PATH \\
 	  GOTOOLCHAIN=$GOTOOLCHAIN \\
 	  GOLANG_VERSION=$GOLANG_VERSION
 	EOF
-  info "fix go environment variables in ssh session."
+  info "fix environment variables in ssh session."
 fi
