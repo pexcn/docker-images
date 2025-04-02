2f71d4ba03ac59564e2406afbc11f62bdccfdc2c
pexcn <pexcn97@gmail.com>
2025-04-02 11:40:42 +0800

    dev: go/rust: use android way to fix environment variables

--- dev/rust/rust.sh
+++ dev/rust/rust.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
-# fix rust environment variables in ssh session
 [ "$ENABLE_SSHD" = 1 ] || exit 0
+
+# fix environment variables in ssh session
 if [ ! -f /etc/profile.d/env_rust.sh ]; then
   cat <<- EOF > /etc/profile.d/env_rust.sh
 	export RUSTUP_HOME=$RUSTUP_HOME \\
 	  CARGO_HOME=$CARGO_HOME \\
-	  PATH=/usr/local/cargo/bin:\$PATH \\
+	  PATH=$PATH \\
 	  RUST_VERSION=$RUST_VERSION
 	EOF
-  info "fix rust environment variables in ssh session."
+  info "fix environment variables in ssh session."
 fi
