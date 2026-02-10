b5ff8a906848b7221d912f2603a9447dfb7190cf
pexcn <pexcn97@gmail.com>
2026-02-11 00:53:26 +0800

    net/udp2raw: update runtime image to 3.23

--- net/udp2raw/udp2raw.sh
+++ net/udp2raw/udp2raw.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-
 # shellcheck disable=SC2155,SC3043
 
 setup_iptables() {
   local symlinks="iptables iptables-save iptables-restore ip6tables ip6tables-save ip6tables-restore"
   local bindir="$(dirname "$(which iptables)")"
+  local backend="$(iptables -V | awk -F'[()]' '{print $2}')"
   if [ "$USE_IPTABLES_NFT_BACKEND" = 1 ]; then
-    for symlink in ${symlinks}; do ln -sf xtables-nft-multi "${bindir}/${symlink}"; done
+    [ "$backend" = "nf_tables" ] || for symlink in ${symlinks}; do ln -sf xtables-nft-multi "${bindir}/${symlink}"; done
   else
-    for symlink in ${symlinks}; do ln -sf xtables-legacy-multi "${bindir}/${symlink}"; done
+    [ "$backend" = "legacy" ] || for symlink in ${symlinks}; do ln -sf xtables-legacy-multi "${bindir}/${symlink}"; done
   fi
 }
 
