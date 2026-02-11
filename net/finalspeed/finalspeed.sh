76d2e946010a010b81abe39fa9ab45b4d3bfa3a9
pexcn <pexcn97@gmail.com>
2026-02-11 15:31:25 +0800

    net/finalspeed: fix iptables on legacy backend systems

--- net/finalspeed/finalspeed.sh
+++ net/finalspeed/finalspeed.sh
@@ -43,6 +43,17 @@ _custom_server_port() {
   echo "$1" > /fs/cnf/listen_port
 }
 
+setup_iptables() {
+  local symlinks="iptables iptables-save iptables-restore ip6tables ip6tables-save ip6tables-restore"
+  local bindir="$(dirname "$(which iptables)")"
+  local backend="$(iptables -V | awk -F'[()]' '{print $2}')"
+  if [ "$USE_IPTABLES_NFT_BACKEND" = 1 ]; then
+    [ "$backend" = "nf_tables" ] || for symlink in ${symlinks}; do ln -sf xtables-nft-multi "${bindir}/${symlink}"; done
+  else
+    [ "$backend" = "legacy" ] || for symlink in ${symlinks}; do ln -sf xtables-legacy-multi "${bindir}/${symlink}"; done
+  fi
+}
+
 start_server() {
   info "start finalspeed server..."
   [ -z "$1" ] || _custom_server_port "$1"
@@ -73,4 +84,5 @@ main() {
   esac
 }
 
+setup_iptables
 main "$@"
