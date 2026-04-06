15b1cb428afeff77ea7e0027ce566abc92a259e2
pexcn <pexcn97@gmail.com>
2026-04-06 14:57:18 +0800

    utils/nezha: remove dashboard unused flags

--- utils/nezha/nezha.sh
+++ utils/nezha/nezha.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
 if [ "$#" -eq 0 ]; then
-  exec dashboard -c /opt/nezha/dashboard.yml -db /opt/nezha/dashboard.db \
-    -loggerLevel WARN \
-    -loggerTimezone "${TZ:-Asia/Taipei}" \
-    -denyQueryTracing
+  exec dashboard
 else
   exec "$@"
 fi
