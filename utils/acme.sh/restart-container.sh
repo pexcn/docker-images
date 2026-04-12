972100d77ddd2136a2c29a8842173c0b7a60cdc7
pexcn <pexcn97@gmail.com>
2026-04-12 18:26:58 +0800

    utils/acme.sh: update to 3.1.2

--- utils/acme.sh/restart-container.sh
+++ utils/acme.sh/restart-container.sh
@@ -7,6 +7,7 @@
 #
 # https://github.com/acmesh-official/acme.sh/blob/5b8d7a3/deploy/docker.sh
 #
+# shellcheck disable=SC2155,SC3043
 
 DOCKER_DAEMON_SOCKET="/var/run/docker.sock"
 
@@ -21,13 +22,13 @@ _get_container_id_by_name() {
 
 _restart_container_by_id() {
   local container_name="$1"
-  local container_id="$(_get_container_id_by_name $container_name)"
-  curl --silent --unix-socket "$DOCKER_DAEMON_SOCKET" -X POST http://localhost/containers/${container_id}/restart
+  local container_id="$(_get_container_id_by_name "$container_name")"
+  curl --silent --unix-socket "$DOCKER_DAEMON_SOCKET" -X POST "http://localhost/containers/${container_id}/restart"
 }
 
 _restart_container_by_name() {
   local container_name="$1"
-  curl --silent --unix-socket "$DOCKER_DAEMON_SOCKET" -X POST http://localhost/containers/${container_name}/restart
+  curl --silent --unix-socket "$DOCKER_DAEMON_SOCKET" -X POST "http://localhost/containers/${container_name}/restart"
 }
 
 for container_name in "$@"; do
